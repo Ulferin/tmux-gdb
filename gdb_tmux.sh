cf5dab9eb08153a8a0856ba5f84f5bd338910120
Federico Finocchio <federico.fino@gmail.com>
2025-09-22 22:45:47 +0200

    Add program argument

--- gdb_tmux.sh
+++ gdb_tmux.sh
@@ -1,33 +1,40 @@
 # Abort if GDB_TMUX_FILE is not defined
-if [ -Z $GDB_TMUX_FILE ]; then
+if [ -z $GDB_TMUX_FILE ]; then
   echo "Environment variable GDB_TMUX_FILE not defined"
   return
 fi
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <exec>"
+    exit 1
+fi
+
+program=$1
 
 # Create new pane and retrieve its ID
 tmux split-window
 # Did not find a better way to wait for file to be created
 sleep 1
 
-filename="/home/federico/last_pane"
-if [ -e "$filename" ]; then
-  pane=$(cat $filename)
+if [ -e "$GDB_TMUX_FILE" ]; then
+  pane=$(cat $GDB_TMUX_FILE)
   echo "Using $pane for GDB output"
 else
-  echo "File $filename not found"
-  return
+  echo "File $GDB_TMUX_FILE not found"
+  exit 1
 fi
 
-tmux send-keys -t "$pane" "tty > \$GDB_TMUX_FILE" Enter
+tty_file="$GDB_TMUX_FILE.tty"
+tmux send-keys -t "$pane" "tty > $tty_file" Enter
 
-if [ -e "$GDB_TMUX_FILE" ]; then
-  tty_value=$(cat $GDB_TMUX_FILE)
+if [ -e "$tty_file" ]; then
+  tty_value=$(cat $tty_file)
 else
-  echo "Could not find file $GDB_TMUX_FILE"
-  return
+  echo "Could not find file $tty_file"
+  exit 1
 fi
 
 # Let the other pane sleep so shell does not interfere with tty input
 tmux send-keys -t "$pane" "sleep 10000" Enter
 
-gdb -ex "tty $tty_value"
+gdb -ex "tty $tty_value" $program
